ccd9bf80d9f09a5ac60bc8a3facabd3728f4a1e2
tarabren <tarabren1296@gmail.com>
2025-04-07 14:34:15 -0400

    added gene name args

--- scripts/parse_sam.py
+++ scripts/parse_sam.py
@@ -1,60 +1,60 @@
 import re
 import pysam
+import argparse
 
-# Input and output file paths
+parser = argparse.ArgumentParser(description="Parse a SAM file and generate a GTF for a custom gene.")
+parser.add_argument("--gene_name", required=True, help="Name of the custom gene (e.g., GFP_abc)")
+parser.add_argument("--transcript_id", required=True, help="Transcript ID for the gene")
+args = parser.parse_args()
+
+# input and output file names
 sam_file = "custom_aligned.sam"
 gtf_file = "custom_genes.gtf"
-gene_name = "Custom Gene"
-transcript_id = ""
+
+gene_name = args.gene_name
+transcript_id = args.transcript_id
 
 # Function to parse the CIGAR string and extract exon positions
 def parse_cigar(start, cigar):
     exons = []
     pos = start
     exon_start = None
-
-    # Regular expression to extract CIGAR operations
     cigar_tuples = re.findall(r'(\d+)([MIDNSHP=X])', cigar)
 
     for length, op in cigar_tuples:
         length = int(length)
-        
-        if op == 'M':  # Matches (exons)
+        if op == 'M':
             if exon_start is None:
                 exon_start = pos
             pos += length
-        elif op == 'N':  # Skipped region (introns)
+        elif op == 'N':
             if exon_start is not None:
-                exons.append((exon_start, pos - 1))  # Store previous exon
-                exon_start = None  # Reset exon start
-            pos += length  # Move past intron
-        elif op in ['D', 'I']:  # Deletions/Insertions (small changes, usually ignored)
-            pass  
+                exons.append((exon_start, pos - 1))
+                exon_start = None
+            pos += length
+        elif op in ['D', 'I']:
+            pass
     
-    # Capture last exon if applicable
     if exon_start is not None:
         exons.append((exon_start, pos - 1))
-
+    
     return exons
 
-# Read the SAM file
+# Read the SAM file and write the GTF
 samfile = pysam.AlignmentFile(sam_file, "r")
 with open(gtf_file, "w") as gtf_out:
     for read in samfile.fetch():
         chrom = read.reference_name
-        start = read.reference_start + 1  # SAM format is 0-based, GTF is 1-based
+        start = read.reference_start + 1
         cigar = read.cigarstring
         strand = "-" if read.flag & 16 else "+"
-        
-        # Get exon positions from CIGAR
+
         exons = parse_cigar(start, cigar)
-        end = exons[-1][1] if exons else start  # Last exon end position
-        
-        # Write GTF entries
+        end = exons[-1][1] if exons else start
+
         gtf_out.write(f"{chrom}\tCustom\tgene\t{start}\t{end}\t.\t{strand}\t.\tgene_id \"{gene_name}\"; gene_name \"{gene_name}\"; gene_version \"1\"; gene_type \"protein_coding\";\n")
         gtf_out.write(f"{chrom}\tCustom\ttranscript\t{start}\t{end}\t.\t{strand}\t.\tgene_id \"{gene_name}\"; transcript_id \"{transcript_id}\"; transcript_version \"1\"; transcript_type \"protein_coding\"; transcript_name \"{gene_name}\";\n")
-        
-        # Write exon entries
+
         for i, (exon_start, exon_end) in enumerate(exons, start=1):
             gtf_out.write(f"{chrom}\tCustom\texon\t{exon_start}\t{exon_end}\t.\t{strand}\t.\tgene_id \"{gene_name}\"; transcript_id \"{transcript_id}\"; exon_number \"{i}\"; exon_id \"{gene_name}_exon{i}\"; exon_version \"1\";\n")
 
